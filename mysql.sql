01a3d575d895cdfb1d598bdba3c207b777ac8a24
saifali9648 <sali964876@gmail.com>
2023-10-19 22:54:52 +0530

    notes

---
+++ mysql.sql
@@ -0,0 +1,64 @@
+/*===================================LESSION2===========================================
+CRUD OPERATION
+CRUD :- stand for 
+CREATE : CREATION OF NEW RECORD WHICH THOROUGH INSERT COMMAND
+READ   : ONCE A DATA INSERTED IN TABLE WE CAN READ THROUGH  SELECT COMMAND
+UPDATE : SOME RECORD IS ALREADY IN OUR TABLE WE CAN UPDATE THOROUGH UPDATE COMMAND
+DELETE : SOME RECORD IS ALREADY IN OUR TABLE WE CAN DELETE THOROUGH DELETE COMMAND
+
+CREATION of table and INSERT statements
+
+hiring requirement locaction Saharsa
+first CREATE DATABASE*/
+ CREATE DATABASE saif;
+--USE DATABASE
+USE saif;
+--CREATE TABLE 
+CREATE TABLE employee
+(
+ firtname varchar(20),
+ middlename varchar(20),
+ lastname varchar(20),
+ age INT,
+ salary INT,
+ locaction varchar(20)
+);
+
+--after that seeing fields write COMMAND
+
+show TABLE;
+
+--than write COMMAND
+
+--DESCribe table_name
+   DESC employee;
+
+/*AFTER CREATING TABLE WE CAN INSERT DATA INTO TABLE RESPECTIVILY FIELDS table_name
+
+FOR INSETING DATA INTO TABLE COMMAND IS
+
+(recommanded way)*/
+INSERT INTO employee(firstname,middlename,lastname,age,salary,locaction) VALUES('MD','SAIF','ALI',22,10000,'SAHARSA');
+
+
+/*you can also write this way (in this case it will run but not all the time)*/
+INSERT INTO employee VALUES('MD','SAIF','ALI',22,10000,'SAHARSA');
+
+/*if someone don't have middlename than 
+--(recommanded way) or to right way*/
+INSERT INTO employee(firstname,lastname,age,salary,location) VALUES('Rajesh','kumar',32,1100,'Saharsa');
+
+/*(in this case it give you an error)*/
+INSERT INTO employee VALUES('Rajesh','kumar',32,1100,'Saharsa');
+
+--if you want to add ' in your record we have two way to achive this 
+--first is
+INSERT INTO employee(firstname,lastname,age,salary,location) VALUES("Rajes's",'kumar',32,1100,'Saharsa'); 
+
+--sencond way is
+--INSERT INTO employee(firstname,lastname,age,salary,location) VALUES('Rajes\'s','kumar',32,1100,'Saharsa');
+
+--how to insert multiple record
+
+INSERT INTO employee(firstname,middlename,lastname,age,salary,location) VALUES('md','rashid','ali',23,10000,'saharsa'),('r
+akesh','kumar','mehta',34,10000,'saharsa');
